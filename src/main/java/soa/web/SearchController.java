604f05174579a7038df002b92f321dcec98cecea
lAngel <info@langelp.net>
2018-12-12 08:07:02 +0100

    Poll Twitter with a maximum amount of tweets to retrieve

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -26,7 +29,10 @@ public class SearchController {
 
   @RequestMapping(value = "/search")
   @ResponseBody
-  public Object search(@RequestParam("q") String q) {
-    return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+  public Object search(@RequestParam("q") String q, @RequestParam(name = "max", defaultValue = "10", required = false) int max) {
+    Map<String, Object> headers = new HashMap<>();
+    headers.put("CamelTwitterKeywords", q);
+    headers.put("CamelTwitterCount", max);
+    return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
   }
 }
--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
 
 function registerSearch() {
     $("#search").submit(function (ev) {
-        event.preventDefault();
-        $.get($(this).attr('action'), {q: $("#q").val()}, function (data) {
+        ev.preventDefault();
+        $.get($(this).attr('action'), {q: $("#q").val(), max: 1}, function (data) {
             $("#resultsBlock").html(Mustache.render(template, data));
         });
     });
